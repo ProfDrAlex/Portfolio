344b003f98aa4a284870b3f435be01fe9d31242e
Alexander Düsterbeck <AlexDuesterbeck@gmail.com>
2023-10-16 21:59:32 +0200

    Routes Config / App.js routes fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {routes} from './components/config/routes/routesConfigMain';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/template-parts/Header/Header';
-import Navigation from './components/template-parts/Navigation/Navigation'; // <-- Import here
-import Home from './components/pages/Home/Home';
-import Contact from './components/pages/Contact/Contact';
+import Navigation from './components/template-parts/Navigation/Navigation';
 import Footer from './components/template-parts/Footer/Footer';
 
 function App() {
     return (
         <Router> {/* <-- Make sure Router wraps your app */}
             <div id="main-content">
-                <Navigation />
-                <Header />
+                <Navigation/>
+                <Header/>
                 <Routes>
+                    {routes.map((route, index) => (
+                        <Route key={index} path={route.path} element={React.createElement(route.component)}/>
+                    ))}
+                </Routes>
+   {/*             <Routes>
                     <Route path="/" element={<Home />}/>
                     <Route path="/contact" element={<Contact />} />
-                </Routes>
-                <Footer />
+                </Routes>*/}
+                <Footer/>
             </div>
         </Router>
     );
--- client/src/components/config/routes/routesConfigMain.js
+++ client/src/components/config/routes/routesConfigMain.js
@@ -1,6 +1,8 @@
 // routesConfig.js
+import Home from '../../pages/Home/Home';
+import Contact from '../../pages/Contact/Contact';
+
 export const routes = [
-    { path: '/', name: 'Home', component: 'HomeComponent' },
-    { path: '/contact', name: 'Contact', component: 'ContactComponent' },
-    // add more routes here
+    { path: '/', name: 'Home', component: Home },
+    { path: '/contact', name: 'Contact', component: Contact },
 ];
